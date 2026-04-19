68c970d7969e2987ada94f2925a069ad18ff158f
LienThuan04 <lienthuan2004@gmail.com>
2026-04-19 16:51:09 +0700

    add refresh token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,24 @@
-import { Body, Controller, Post, Res, UseGuards } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from '@/auth/auth.service';
 import { Public } from '@/lib/decorator/metadata';
 import { LoginDto, RegisterDto } from '@/auth/dto/create-auth.dto';
-import type { Response } from 'express';
+import type { Request, Response } from 'express';
 import { User } from '@/lib/decorator/user.decorator';
 import { UserEntity } from '@/users/entities/user.entity';
 import { LocalAuthGuard } from '@/lib/passport/local-auth.guard';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {
+    private readonly refreshTokenName: string;
+    constructor(
+        private readonly authService: AuthService,
+        private readonly ConfigService: ConfigService
+    ) {
+        this.refreshTokenName = this.ConfigService.get<string>('NAME_COOKIE_REFRESH_TOKEN_BROWSER')!;
+        if (!this.refreshTokenName || this.refreshTokenName.trim() === '') {
+            throw new Error('Refresh token cookie name is not defined in environment variables');
+        }
 
     }
 
@@ -30,7 +39,7 @@ export class AuthController {
     @UseGuards(LocalAuthGuard) // Apply the local authentication guard to this route
     @Post('login')
     async login(@Res({ passthrough: true }) res: Response, @User() user: UserEntity, @Body() loginDto: LoginDto ) {
-        const result = await this.authService.login(user, res, loginDto);
+        const result = await this.authService.login(user, res, loginDto.deviceId);
         return {
             message: 'Login successful',
             accessToken: result.accessToken,
@@ -38,5 +47,20 @@ export class AuthController {
         };
     }
 
+
+    @Public()
+    @Post('refresh')
+    async refreshToken(@Res({ passthrough: true }) res: Response, @Req() req: Request ){
+        const oldCookieRefreshToken = req.cookies[this.refreshTokenName];
+        if (!oldCookieRefreshToken) {
+            throw new BadRequestException('Refresh token is missing in cookies');
+        }
+        const result = await this.authService.refreshToken(oldCookieRefreshToken, res);
+        return {
+            message: 'Token refreshed successfully',
+            accessToken: result.accessToken,
+            user: result.user,
+        };
+    }
 
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -15,6 +15,11 @@ import { CreateSessionDto } from '@/session/dto/create-session.dto';
 
 @Injectable()
 export class AuthService {
+    private readonly refreshTokenName: string;
+    private readonly expiresInRefresh: string;
+    private readonly refreshTokenSecret: string;
+    private readonly defaultRoleName: string;
+
     constructor(
         private readonly prismaService: PrismaService,
         private readonly jwtService: JwtService,
@@ -22,12 +27,26 @@ export class AuthService {
         private readonly usersService: UsersService,
         private readonly sessionService: SessionService
 
-    ) { }
-    private readonly refresh_token: string = "refresh_token";
+    ) {
+        this.refreshTokenName = this.configService.get<string>('NAME_COOKIE_REFRESH_TOKEN_BROWSER')!;
+        this.expiresInRefresh = this.configService.get<string>('JWT_REFRESH_EXPIRE')!;
+        this.refreshTokenSecret = this.configService.get<string>('JWT_REFRESH_TOKEN_SECRET')!;
+        if (!this.refreshTokenName || this.refreshTokenName.trim() === '') {
+            throw new Error('Refresh token cookie name is not defined in environment variables');
+        }
+        if (!this.expiresInRefresh || this.expiresInRefresh.trim() === '') {
+            throw new Error('JWT refresh token expiration is not defined in environment variables');
+        }
+        if (!this.refreshTokenSecret || this.refreshTokenSecret.trim() === '') {
+            throw new Error('JWT refresh token secret is not defined in environment variables');
+        }
+
+        this.defaultRoleName = this.configService.get<string>('NAME_ROLE_USER') || 'USER';
+    }
 
     private async generateRefreshToken(payload: { userId: string; _sub: string, deviceId: string }): Promise<string> {
-        const expiresIn = this.configService.get<string>('JWT_REFRESH_EXPIRE');
-        const secret = this.configService.get<string>('JWT_REFRESH_TOKEN_SECRET');
+        const expiresIn = this.expiresInRefresh;
+        const secret = this.refreshTokenSecret;
         if (!expiresIn || !secret) {
             throw new Error('JWT refresh token or secret is not defined in environment variables');
         }
@@ -37,12 +56,12 @@ export class AuthService {
 
     async validateUser(userNameOrEmail: string, password: string) {
         const user = await this.usersService.searchUserByEmailOrUsername(userNameOrEmail);
-        if(!user){
+        if (!user) {
             return null;
         }
         const getRoleUser = user.roleId ? await this.prismaService.role.findUnique({ where: { id: user.roleId.toString() } }) : null;
         const authPass = await comparePassword(password, user.password);
-        if(!authPass){
+        if (!authPass) {
             return null;
         }
         return plainToInstance(UserEntity, { ...user, roleName: getRoleUser ? getRoleUser.roleName : null }, { excludeExtraneousValues: false });
@@ -55,7 +74,7 @@ export class AuthService {
                 email,
                 userName,
                 password,
-                roleName: this.configService.get<string>('NAME_ROLE_USER') || 'USER'
+                roleName: this.defaultRoleName
             };
             const result = await this.usersService.create(CreateUserDto);
             return result;
@@ -65,37 +84,66 @@ export class AuthService {
         }
     }
 
-    async login(user: UserEntity, res: Response, loginDto: LoginDto) {
-        const refreshToken = await this.generateRefreshToken({ userId: user.id, _sub: user.roleName || user.email, deviceId: loginDto.deviceId });
-        const createSessionDto: CreateSessionDto = {
-            userId: user.id,
-            refreshToken,
-            deviceId: loginDto.deviceId
-        };
-        const setSessionDB = await this.sessionService.upsertSession(createSessionDto);
-        if (!setSessionDB) {
-            throw new BadRequestException('Failed to create or update session in database');
+    async login(user: UserEntity, res: Response, deviceId: string): Promise<{ accessToken: string; user: Omit<UserEntity, 'password' | 'createdAt' | 'updatedAt'> }> {
+        try {
+            const refreshToken = await this.generateRefreshToken({ userId: user.id, _sub: user.roleName || user.email, deviceId });
+            const createSessionDto: CreateSessionDto = {
+                userId: user.id,
+                refreshToken,
+                deviceId
+            };
+            const setSessionDB = await this.sessionService.upsertSession(createSessionDto);
+            if (!setSessionDB) {
+                throw new BadRequestException('Failed to create or update session in database');
+            }
+            const expiresInRefreshToken = this.expiresInRefresh;
+            if (!expiresInRefreshToken) {
+                throw new Error('JWT refresh token expiration is not defined in environment variables');
+            }
+            res.cookie(this.refreshTokenName, refreshToken, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'strict',
+                maxAge: ms(expiresInRefreshToken as ms.StringValue) // Set cookie expiration to match refresh token expiration
+            })
+            const payload: Omit<UserEntity, 'password' | 'createdAt' | 'updatedAt'> = {
+                id: user.id,
+                email: user.email,
+                avatarUrl: user.avatarUrl,
+                backgroundUrl: user.backgroundUrl,
+                description: user.description,
+                userName: user.userName,
+                roleId: user.roleId,
+                roleName: user.roleName
+            }
+            return { accessToken: this.jwtService.sign(payload), user: payload };
+        } catch (error) {
+            throw new BadRequestException('Failed to login user', (error as Error).message);
         }
-        const expiresInRefreshToken = this.configService.get<string>('JWT_REFRESH_EXPIRE');
-        if (!expiresInRefreshToken) {
-            throw new Error('JWT refresh token expiration is not defined in environment variables');
+    }
+
+    async refreshToken(oldCookieRefreshToken: string, res: Response) {
+        try {
+            if (!oldCookieRefreshToken || oldCookieRefreshToken.trim() === '' || oldCookieRefreshToken === 'undefined') {
+                throw new BadRequestException('Refresh token is missing !');
+            }
+            const decodedRefreshToken = this.jwtService.verify(oldCookieRefreshToken, { secret: this.refreshTokenSecret });
+            if (!decodedRefreshToken || typeof decodedRefreshToken === 'string' || !decodedRefreshToken.userId || !decodedRefreshToken._sub || !decodedRefreshToken.deviceId) {
+                throw new BadRequestException('Invalid refresh token payload');
+            }
+            const session = await this.sessionService.findSessionByRefreshTokenAndDeviceId(oldCookieRefreshToken,  decodedRefreshToken.deviceId);
+            if (!session) {
+                throw new BadRequestException('Invalid refresh token or session not found');
+            }
+            const userFetch = await this.prismaService.user.findUnique({ where: { id: session.userId }, include: { role: true } });
+            if (!userFetch || userFetch.id !== decodedRefreshToken.userId) {
+                throw new BadRequestException('User not found for the given refresh token');
+            }
+            const userEntity = plainToInstance(UserEntity, { ...userFetch, roleName: userFetch.role ? userFetch.role.roleName : null }, { excludeExtraneousValues: false });
+            res.clearCookie(this.refreshTokenName); // Clear old refresh token cookie
+            return await this.login(userEntity, res, decodedRefreshToken.deviceId); // Reuse login logic to generate new tokens and set cookie
+        } catch (error: any) {
+            throw new BadRequestException('Failed to refresh token', error.message);
         }
-        res.cookie( this.refresh_token, refreshToken, {
-            httpOnly: true,
-            secure: true,
-            sameSite: 'strict',
-            maxAge: ms(expiresInRefreshToken as ms.StringValue) // Set cookie expiration to match refresh token expiration
-        })
-        const payload: Omit<UserEntity, 'password' | 'createdAt' | 'updatedAt'> = {
-            id: user.id,
-            email: user.email,
-            avatarUrl: user.avatarUrl,
-            backgroundUrl: user.backgroundUrl,
-            description: user.description,
-            userName: user.userName,
-            roleId: user.roleId,
-            roleName: user.roleName
-        }
-        return { accessToken: this.jwtService.sign(payload), user: payload };
     }
 }

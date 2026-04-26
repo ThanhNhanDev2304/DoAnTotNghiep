9e536f298e90ef4185cfc308cbf9ff2e7c6b2474
LienThuan04 <lienthuan2004@gmail.com>
2026-04-26 14:07:59 +0700

    add table PendingRegistration

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger"
-import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator"
+import { IsEmail, IsNotEmpty, IsString, Matches, MaxLength, MinLength } from "class-validator"
 
 export class RegisterDto {
 
@@ -8,6 +8,7 @@ export class RegisterDto {
     @IsString({ message: 'Username must be a string' })
     @MaxLength(100, { message: 'Username must be at most 100 characters long' })
     @MinLength(3, { message: 'Username must be at least 3 characters long' })
+    @Matches(/^[a-zA-Z0-9_]+$/, { message: 'Username can only contain letters, numbers and underscore' })
     userName!: string
 
     @ApiProperty({ example: 'example@gmail.com', description: 'The email of the new user' })
